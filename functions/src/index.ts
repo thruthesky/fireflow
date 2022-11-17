01a01d8b21351b41c6391a80b9b72dcafa0df7b6
lancelynyrd <pinedaclp@gmail.com>
2022-11-17 10:53:05 +0800

    remove test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,5 +6,5 @@ admin.firestore().settings({ ignoreUndefinedProperties: true });
 
 export * from "./controllers/user.functions";
 export * from "./controllers/forum.functions";
-// export * from "./controllers/chat.functions";
+export * from "./controllers/chat.functions";
 export * from "./controllers/messaging.functions";
