6744be13fa815faec13d4e113054688a38e70b58
JaeHo Song <thruthesky@gmail.com>
2022-11-17 12:11:36 +0800

    comment document reference

--- functions/src/models/comment.model.ts
+++ functions/src/models/comment.model.ts
@@ -22,16 +22,17 @@ export class Comment {
    * @returns Returns the uid of ancestors.
    */
   static async getAncestorsUid(
-      commentId: string,
-      authorUid?: string
+    commentId: string,
+    authorUid?: string
   ): Promise<string[]> {
     let comment = await Comment.get(commentId);
     const uids = [comment?.userDocumentReference.id];
     while (
-      comment?.parentCommentReference &&
-      comment.postDocumentReference.id != comment.parentCommentReference.id
+      comment?.parentCommentDocumentReference &&
+      comment.postDocumentReference.id !=
+        comment.parentCommentDocumentReference.id
     ) {
-      comment = await Comment.get(comment!.parentCommentReference.id);
+      comment = await Comment.get(comment!.parentCommentDocumentReference.id);
       if (comment == null) break;
       uids.push(comment?.userDocumentReference.id);
     }
@@ -53,13 +54,13 @@ export class Comment {
   static async updateMeta(comment: CommentDocument, commentId: string) {
     const post = await Post.get(comment.postDocumentReference.id);
     let parent;
-    if (comment.parentCommentReference) {
-      parent = await Comment.get(comment.parentCommentReference.id);
+    if (comment.parentCommentDocumentReference) {
+      parent = await Comment.get(comment.parentCommentDocumentReference.id);
     }
     const order = Utils.commentOrder(
-        parent?.order,
-        parent?.depth,
-        post.noOfComments
+      parent?.order,
+      parent?.depth,
+      post.noOfComments
     );
     return Ref.commentDoc(commentId).update({
       depth: parent?.depth ? parent.depth + 1 : 1,
