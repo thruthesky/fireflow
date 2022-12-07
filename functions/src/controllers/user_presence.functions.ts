cc6b73762a3e1d9fc7c18382d27a116b18f6cfd4
JaeHo Song <thruthesky@gmail.com>
2022-12-07 15:24:13 +0800

    update. chat

--- functions/src/controllers/user_presence.functions.ts
+++ functions/src/controllers/user_presence.functions.ts
@@ -6,29 +6,29 @@ const firestore = admin.firestore();
 // Realtime database 에서 /status/<uid> 가 변경되면, Firestore 로 sync 하는 역할을 한다.
 // 특히, 사용자가 offline 이 될 때, 필요하다.
 export const onUserStatusChanged = functions.database
-  .ref("/status/{uid}")
-  .onUpdate(async (change: any, context: any) => {
+    .ref("/status/{uid}")
+    .onUpdate(async (change: any, context: any) => {
     // 저장된 데이터를 가져온다.
-    const eventStatus = change.after.val();
+      const eventStatus = change.after.val();
 
-    // 사용자 공개 문서
-    const userStatusFirestoreRef = firestore.doc(
-      `users_public_data/${context.params.uid}`
-    );
+      // 사용자 공개 문서
+      const userStatusFirestoreRef = firestore.doc(
+          `users_public_data/${context.params.uid}`
+      );
 
-    // 현재 onUpdate() 이벤트가 이전 업데이트로 재귀적으로 덮어쓰여졌을 수 있으므로,
-    // 확실히 하기 위해서, 데이터를 한번 더 읽어서, timestamp 를 비교한다.
-    const statusSnapshot = await change.after.ref.once("value");
-    const status = statusSnapshot.val();
-    functions.logger.log(status, eventStatus);
-    // 현재 이벤트 문서의 시간 값이, 이전에 발생한 이벤트 문서의 시간 보다 크면, 종료.
-    if (status.last_changed > eventStatus.last_changed) {
-      return null;
-    }
+      // 현재 onUpdate() 이벤트가 이전 업데이트로 재귀적으로 덮어쓰여졌을 수 있으므로,
+      // 확실히 하기 위해서, 데이터를 한번 더 읽어서, timestamp 를 비교한다.
+      const statusSnapshot = await change.after.ref.once("value");
+      const status = statusSnapshot.val();
+      functions.logger.log(status, eventStatus);
+      // 현재 이벤트 문서의 시간 값이, 이전에 발생한 이벤트 문서의 시간 보다 크면, 종료.
+      if (status.last_changed > eventStatus.last_changed) {
+        return null;
+      }
 
-    // Firestore 에 맞게 Timestamp 를 변경
-    eventStatus.last_changed = new Date(eventStatus.last_changed);
+      // Firestore 에 맞게 Timestamp 를 변경
+      eventStatus.last_changed = new Date(eventStatus.last_changed);
 
-    // Firestore /users_public_data/<uid> 에 상태 업데이트
-    return userStatusFirestoreRef.set(eventStatus, { merge: true });
-  });
+      // Firestore /users_public_data/<uid> 에 상태 업데이트
+      return userStatusFirestoreRef.set(eventStatus, { merge: true });
+    });
--- functions/src/models/chat.model.ts
+++ functions/src/models/chat.model.ts
@@ -23,9 +23,9 @@ export class Chat {
    * @param data
    *
    * 참고, 채팅방 정보 문서(/chat_rooms/<docId>)는 사용자가 채팅방에 입장 할 때, ChatRoomMessages 위젯에서 생성된다.
-   * 그래서, 여기서 따로 채팅방이 존재하지 않으면 생성 할 필요는 없지만,
-   * 사용자가 가입하면 웰컴 메시지를 보내는 경우에는, 채팅방을 생성해야 새 메시지 수 (1) 이 사용자 화면에 표시된다.
-   * 그래서 set(merge: true) 로 채팅방을 업데이트 하는 것이다.
+   * 그래서, 여기서 따로 채팅방이 존재하는지 하지 않는지 확인 할 필요 없다.
+   *
+   * 채팅 방의 사용자 추가, 삭제는 클라이언트에서 이루어져야 한다.
    */
   static async updateRoom(
       data: ChatMessageDocument
@@ -37,12 +37,6 @@ export class Chat {
       last_message_sent_by: data.senderUserDocumentReference,
       last_message_seen_by: [data.senderUserDocumentReference],
     } as ChatRoomDocument;
-    // 1:1 채팅이면, users 배열 추가. (웰컴 메시지에서 필요)
-    // 검토, 어쩌면, 웰컴 메시지의 경우, 따로 로직을 분리하는 것이 필요할 수 있겠다.
-    if (data.chatRoomDocumentReference.id.indexOf("-") > 0) {
-      const arr = data.chatRoomDocumentReference.id.split("-");
-      info.users = [Ref.userDoc(arr[0]), Ref.userDoc(arr[1])];
-    }
     return data.chatRoomDocumentReference.set(info, { merge: true });
   }
 
@@ -57,6 +51,7 @@ export class Chat {
     if (system.helperUid && system.welcomeMessage) {
       const chatRoomId = [system.helperUid, uid].sort().join("-");
 
+      // 웰컴 메시지를 채팅방 메세지로 기록
       const message: ChatMessageDocument = {
         chatRoomDocumentReference: Ref.chatRoomsCol.doc(chatRoomId),
         senderUserDocumentReference: Ref.userDoc(system.helperUid),
@@ -65,13 +60,32 @@ export class Chat {
       };
       await Ref.chatRoomMessagesCol.add(message);
 
-      await this.updateRoom(message);
+      // * 사용자가 가입하면 웰컴 메시지를 보내는 경우에는, 채팅방을 생성해야 새 메시지 수 (1) 이 사용자 화면에 표시된다.
+      // * 그래서 set(merge: true) 로 채팅방을 업데이트 하는 것이다.
+      // *
+      // await this.updateRoom(message);
+      // 채팅방 정보 업데이트
+      const info: ChatRoomDocument = {
+        last_message: message.text,
+        last_message_timestamp: message.timestamp,
+        last_message_sent_by: message.senderUserDocumentReference,
+        last_message_seen_by: [message.senderUserDocumentReference],
+        users: [message.senderUserDocumentReference, Ref.userDoc(uid)],
+      } as ChatRoomDocument;
+      // 1:1 채팅이면, users 배열 추가.
+      await message.chatRoomDocumentReference.set(info, { merge: true });
+
       return chatRoomId;
     } else {
       return null;
     }
   }
 
+  /**
+   * 채팅 메시지를 받아서, 채팅방 정보를 보고, 현재 메시지를 보낸 사용자를 제외한 나머지 사용자들의 UID 를 리턴한다.
+   * @param data chat room message document
+   * @returns array of other user uids
+   */
   static async getOtherUserUidsFromChatMessageDocument(
       data: ChatMessageDocument
   ): Promise<string> {
