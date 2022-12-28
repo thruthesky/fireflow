69798c12fa73ff1767f022874c6f871d346a6a7b
JaeHo Song <thruthesky@gmail.com>
2022-12-28 17:00:55 +0800

    api keys

--- functions/src/models/gpt.model.ts
+++ functions/src/models/gpt.model.ts
@@ -1,30 +1,31 @@
 import * as admin from "firebase-admin";
 import { Configuration, OpenAIApi } from "openai";
-import { SystemConfig } from "../system.config";
 import { GptDocument } from "../interfaces/gpt.interface";
+
+import keys from "../../../keys/keys.json";
 
 export class Gpt {
   static async query(
-      data: GptDocument,
-      ref: FirebaseFirestore.DocumentReference
+    data: GptDocument,
+    ref: FirebaseFirestore.DocumentReference
   ): Promise<void> {
     const configuration = new Configuration({
-      apiKey: SystemConfig.openAiKey,
+      apiKey: keys.openAiKey,
     });
 
     const openai = new OpenAIApi(configuration);
     const queryStartedAt = admin.firestore.FieldValue.serverTimestamp();
     try {
       const completion = await openai.createCompletion(
-          {
-            model: data.model,
-            prompt: data.prompt,
-            max_tokens: data.max_tokens,
-            temperature: data.temperature,
-          },
-          {
-            timeout: 60000,
-          }
+        {
+          model: data.model,
+          prompt: data.prompt,
+          max_tokens: data.max_tokens,
+          temperature: data.temperature,
+        },
+        {
+          timeout: 60000,
+        }
       );
       console.log(completion.data.choices[0].text);
       await ref.update({
--- functions/src/system.config.ts
+++
@@ -1,9 +0,0 @@
-export class SystemConfig {
-  private static _openAiKeyHeader: string = "sk-";
-  private static _openAiKeyBody: string =
-    "D5i9Dfi9cZOfIrkAIMoeT3BlbkFJixu2HASnqUigvQq";
-  private static _openAiKeyFooter: string = "uxuyJ";
-  public static get openAiKey(): string {
-    return this._openAiKeyHeader + this._openAiKeyBody + this._openAiKeyFooter;
-  }
-}
